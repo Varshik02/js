c533fa6d09ee84a381a7ee8f2041028c8aeaeba1
Varshik Prajapati <151313251+Varshik02@users.noreply.github.com>
2024-06-05 07:56:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use Strict"; //treate all js code as newer version
+
+//alert(3 + 3) // we are using nodejs not browser
+
+console.log(3 + 
+    3) // code redabilit should be high
+console.log("varshik")
+
+let name = "varshik"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint (used for bignumbers. example:- stock market, treading)
+// string => '', ""
+// boolean => true, false
+// null => standAlone value
+// undefined => value not assigned
+// symbol => uniquness
+
+
+//object
+
+console.log(typeof "varshik")
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
