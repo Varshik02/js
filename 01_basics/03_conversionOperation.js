77c1b9dd684b60b2c0eb73a87c93d653c276c4fd
Varshik Prajapati <151313251+Varshik02@users.noreply.github.com>
2024-06-05 08:16:42 +0000

    conversionOfDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "varshik"
+
+// // console.log(typeof score);
+// // console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "varshik"
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"varshik" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
