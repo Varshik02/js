981dd84a8adcaaeb21836a7fc474dd7f7948e98b
Varshik Prajapati <151313251+Varshik02@users.noreply.github.com>
2024-06-08 18:48:07 +0000

    IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,8 +1,12 @@
 // Immedietly Invoked Function Expression (IIFE)
 
 (function chai(){
+    // named iife
     console.log(`DB CONNECTED`);
-})()
+})();
 
+( (name) => {
+    console.log(`Db connected two ${name}`);
+})('varshik')
 
 
