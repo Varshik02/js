e27af9c7984b55e95b9cc34c28c5a259a30c0f31
Varshik Prajapati <151313251+Varshik02@users.noreply.github.com>
2024-06-10 09:20:57 +0000

    loops

---
+++ 05_iterations/06.js
@@ -0,0 +1,23 @@
+// const coding = ["js", "Ruby", "java", "c++", "cpp"]
+
+// const values = coding.forEach( (iteam) => {
+//     //console.log(iteam);
+//     return iteam
+// })
+
+// console.log(values); 
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 5
+// })
+
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num > 4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
